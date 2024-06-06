32018899ab71b7112f72aa9d91a9483c4f535909
Jeremy Marcus <jeremy.i.marcus@gmail.com>
2024-06-06 12:24:50 -0700

    split top-level method into parts

--- dimelo/plot_read_browser.py
+++ dimelo/plot_read_browser.py
@@ -65,6 +65,53 @@ def plot_read_browser(
         calculate_mod_fractions=False,
     )
 
+    read_extent_df, mod_event_df = format_browser_data(
+        read_tuples=read_tuples, entry_labels=entry_labels
+    )
+
+    # Apply threshold to mod_event_df
+    if thresh is not None:
+        mod_event_df = mod_event_df[mod_event_df.prob > utils.adjust_threshold(thresh)]
+    else:
+        # Still need to filter out all values that are effectively 0, or the read bars cannot be seen
+        # TODO: This seems like the wrong place to be handling this.
+        mod_event_df = mod_event_df[mod_event_df.prob > utils.adjust_threshold(2)]
+
+    chrom, (region_start, region_end, _) = utils.parse_region_string(
+        region=region, window_size=None
+    )
+
+    fig = make_browser_figure(
+        read_extent_df=read_extent_df,
+        mod_event_df=mod_event_df,
+        collapse=collapse,
+        meta_sort=meta_sort,
+        chrom=chrom,
+        region_start=region_start,
+        region_end=region_end,
+    )
+
+    return fig
+
+
+def format_browser_data(
+    read_tuples: list[tuple],
+    entry_labels: list[str],
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Take data from load_processed.read_vectors_from_hdf5() and format it for browser plotting.
+
+    Argument descriptions taken directly from documentation of load_processed.read_vectors_from_hdf5().
+
+    Args:
+        read_tuples: a list of tuples, each tuple containing all datasets corresponding to an individual read that
+        was within the specified regions.
+        entry_labels: a list of strings, naming the datasets returned.
+
+    Returns:
+        * dataframe defining the start, end, name, and desired y-index (sorting) of each read
+        * dataframe defining all necessary information to place each modification event on the browser
+    """
     # Coerce read tuples to initial dataframe, throwing away unnecessary columns
     to_exclude = ["chromosome", "strand", "region_start", "region_end"]
     read_df = pd.DataFrame.from_records(
@@ -102,80 +149,7 @@ def plot_read_browser(
         .rename(columns={"pos_vector": "pos", "prob_vector": "prob"})
     )
 
-    if collapse:
-        index_map = collapse_rows(read_extent_df, meta_sort=meta_sort)
-        read_extent_df["y_index"] = read_extent_df["y_index"].map(index_map)
-        mod_event_df["y_index"] = mod_event_df["y_index"].map(index_map)
-
-    # Apply threshold to mod_event_df
-    if thresh is not None:
-        mod_event_df = mod_event_df[mod_event_df.prob > utils.adjust_threshold(thresh)]
-    else:
-        # Still need to filter out all values that are effectively 0, or the read bars cannot be seen
-        # TODO: This seems like the wrong place to be handling this.
-        mod_event_df = mod_event_df[mod_event_df.prob > utils.adjust_threshold(2)]
-
-    # Build final figure
-    # TODO: Enable setting some relevant parameters
-    chrom, (region_start, region_end, _) = utils.parse_region_string(
-        region=region, window_size=None
-    )
-    # TODO: Understand all of the options here; are they all as desired?
-    layout = plotly.graph_objs.Layout(
-        barmode="overlay",
-        title=chrom,
-        hovermode="closest",
-        plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=dict(range=[region_start, region_end]),
-    )
-    # TODO: I feel like there has to be a cleaner way to do this, maybe using plotly express, but I dont know and I'm just trying to get this done first. Lots of iterrows. Sad.
-    fig = plotly.graph_objects.Figure(layout=layout)
-    for _, row in read_extent_df.iterrows():
-        fig.add_trace(
-            plotly.graph_objects.Scatter(
-                x=[row.read_start, row.read_end],
-                y=[row.y_index, row.y_index],
-                mode="lines",
-                line=dict(width=1, color="lightgrey"),
-                showlegend=False,
-                hoverinfo="text",
-                hovertext=row.read_name,
-            )
-        )
-    for motif_idx, (motif, motif_df) in enumerate(mod_event_df.groupby("motif")):
-        min_overall = motif_df["prob"].min()
-        max_overall = motif_df["prob"].max()
-        fig.add_trace(
-            plotly.graph_objs.Scatter(
-                x=motif_df["pos"],
-                y=motif_df["y_index"],
-                mode="markers",
-                showlegend=False,
-                hoverinfo="text",
-                hovertext=motif_df["read_name"],
-                marker=dict(
-                    size=4,
-                    color=motif_df["prob"],
-                    colorscale=utils.DEFAULT_COLORSCALES[motif],
-                    colorbar=dict(
-                        title=f"{motif} probability",
-                        titleside="right",
-                        tickmode="array",
-                        tickvals=[min_overall, max_overall],
-                        ticktext=[
-                            str(round(min_overall, 2)),
-                            str(round(max_overall, 2)),
-                        ],
-                        ticks="outside",
-                        thickness=15,
-                        # TODO: Is this positioning system dumb?
-                        x=1 + (motif_idx * 0.10),
-                    ),
-                ),
-            )
-        )
-
-    return fig
+    return read_extent_df, mod_event_df
 
 
 def collapse_rows(
@@ -201,7 +175,7 @@ def collapse_rows(
     This series can be applied to the original data by using the pd.Series.map() method.
 
     Args:
-        read_extent_df: dataframe with the columns ["read_start", "read_end", "y_index"]
+        read_extent_df: read extent dataframe from format_browser_data()
         minimum_gap: minimum number of bases allowed between the end of one read and the beginning of
             the next for the two reads to be placed on the same row
         meta_sort: type of meta sorting to do; one of ["full_extent", "covered_bases", None]
@@ -280,6 +254,98 @@ def collapse_rows(
     else:
         # Return Series mapping original indices to collapsed indices
         return idx_map_orig2collapse
+
+
+def make_browser_figure(
+    read_extent_df: pd.DataFrame,
+    mod_event_df: pd.DataFrame,
+    collapse: bool,
+    meta_sort: str,
+    chrom: str,
+    region_start: int,
+    region_end: int,
+) -> plotly.graph_objs.Figure:
+    """
+    Make a browser figure, using the provided pre-processed data
+
+    Args:
+        read_extent_df: read extent dataframe from format_browser_data()
+        mod_event_df: mod event dataframe from format_browser_data()
+        collapse: if True, allows multiple reads on single rows of the browser for a more condensed
+            visualization.
+        meta_sort: additional fine tuning option for read collapsing; ignored when collapse is False; see
+            collapse_rows() for details.
+        chrom: chromosome of the region being browsed
+        region_start: start position of the region being browsed
+        region_end: end position of the region being browsed
+
+    TODO: Think about how this interfaces with different types of initial sorting...
+    TODO: Make it so that this method does NOT modify the input dataframe
+    TODO: Should this method do the collapsing, or should this method require collapsing outside?
+    """
+    if collapse:
+        index_map = collapse_rows(read_extent_df, meta_sort=meta_sort)
+        read_extent_df["y_index"] = read_extent_df["y_index"].map(index_map)
+        mod_event_df["y_index"] = mod_event_df["y_index"].map(index_map)
+
+    # Build final figure
+    # TODO: Enable setting some relevant parameters
+
+    # TODO: Understand all of the options here; are they all as desired?
+    layout = plotly.graph_objs.Layout(
+        barmode="overlay",
+        title=chrom,
+        hovermode="closest",
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=dict(range=[region_start, region_end]),
+    )
+    # TODO: I feel like there has to be a cleaner way to do this, maybe using plotly express, but I dont know and I'm just trying to get this done first. Lots of iterrows. Sad.
+    fig = plotly.graph_objects.Figure(layout=layout)
+    for _, row in read_extent_df.iterrows():
+        fig.add_trace(
+            plotly.graph_objects.Scatter(
+                x=[row.read_start, row.read_end],
+                y=[row.y_index, row.y_index],
+                mode="lines",
+                line=dict(width=1, color="lightgrey"),
+                showlegend=False,
+                hoverinfo="text",
+                hovertext=row.read_name,
+            )
+        )
+    for motif_idx, (motif, motif_df) in enumerate(mod_event_df.groupby("motif")):
+        min_overall = motif_df["prob"].min()
+        max_overall = motif_df["prob"].max()
+        fig.add_trace(
+            plotly.graph_objs.Scatter(
+                x=motif_df["pos"],
+                y=motif_df["y_index"],
+                mode="markers",
+                showlegend=False,
+                hoverinfo="text",
+                hovertext=motif_df["read_name"],
+                marker=dict(
+                    size=4,
+                    color=motif_df["prob"],
+                    colorscale=utils.DEFAULT_COLORSCALES[motif],
+                    colorbar=dict(
+                        title=f"{motif} probability",
+                        titleside="right",
+                        tickmode="array",
+                        tickvals=[min_overall, max_overall],
+                        ticktext=[
+                            str(round(min_overall, 2)),
+                            str(round(max_overall, 2)),
+                        ],
+                        ticks="outside",
+                        thickness=15,
+                        # TODO: Is this positioning system dumb?
+                        x=1 + (motif_idx * 0.10),
+                    ),
+                ),
+            )
+        )
+    return fig
 
 
 def save_static(
