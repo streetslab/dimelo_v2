91fb5e4abc317debb1a03716ec47b7f4c483773a
Oberon Dixon-Luinenburg <dixonluinenburg@n0011.savio3>
2025-01-08 09:40:23 -0800

    DM-229 Adjusted export.pileup_to_bigwig to use new load_processed.process_pileup_row and renamed/configured a few variables to support this.

--- dimelo/export.py
+++ dimelo/export.py
@@ -6,7 +6,7 @@ import pyBigWig
 import pysam
 from tqdm.auto import tqdm
 
-from . import utils
+from . import load_processed, utils
 
 """
 This module contains code to export indexed and compressed parse output files to other formats that may be helpful for downstream analysis.
@@ -57,8 +57,8 @@ def pileup_to_bigwig(
 
     # Because we need to set up the bigwig header for we start writing data to it, we need to pre-index the length of each contig
     tabix = pysam.TabixFile(str(bedmethyl_file))
-    contig_lengths_tuples = []
-    lines_by_contig = {}
+    contig_lengths = {}
+    contig_lines = {}
 
     parsed_motif = utils.ParsedMotif(motif)
 
@@ -84,11 +84,11 @@ def pileup_to_bigwig(
         )[0]
         fields = last_row.split("\t")
         max_coord = int(fields[2])
-        contig_lengths_tuples.append((contig, max_coord))
-        lines_by_contig[contig] = rows_count
+        contig_lengths[contig] = max_coord
+        contig_lines[contig] = rows_count
 
     with pyBigWig.open(str(output_file_path), "w") as bw:
-        bw.addHeader(contig_lengths_tuples)
+        bw.addHeader([(contig, length) for contig, length in contig_lengths.items()])
         for contig in tqdm(
             tabix.contigs,
             desc=f"Step 2: Writing {Path(bedmethyl_file).name} contents to {Path(output_file_path).name}",
@@ -100,57 +100,37 @@ def pileup_to_bigwig(
             for row in tqdm(
                 tabix.fetch(contig),
                 desc=f"Writing {contig}.",
-                total=lines_by_contig[contig],
+                total=contig_lines[contig],
                 leave=False,
             ):
-                # TODO: This code is copied from load_processed.pileup_counts_from_bedmethyl and should probably be consolidated at some point
-                tabix_fields = row.split("\t")
-                pileup_basemod = tabix_fields[3]
-                pileup_strand = tabix_fields[5]
-                keep_basemod = False
-                if (strand != ".") and (pileup_strand != strand):
-                    # This entry is on the wrong strand - skip it
-                    continue
-                elif len(pileup_basemod.split(",")) == 3:
-                    pileup_modname, pileup_motif, pileup_mod_coord = (
-                        pileup_basemod.split(",")
+                keep_basemod, genomic_coord, modified_in_row, valid_in_row = (
+                    load_processed.process_pileup_row(
+                        row=row,
+                        parsed_motif=parsed_motif,
+                        region_start=0,
+                        region_end=contig_lengths[contig],
+                        region_strand=strand,
+                        single_strand=(strand != "."),
+                        regions_5to3prime=False,
                     )
-                    if (
-                        pileup_motif == parsed_motif.motif_seq
-                        and int(pileup_mod_coord) == parsed_motif.modified_pos
-                        and pileup_modname in parsed_motif.mod_codes
-                    ):
-                        keep_basemod = True
-                elif len(pileup_basemod.split(",")) == 1:
-                    if pileup_basemod in parsed_motif.mod_codes:
-                        keep_basemod = True
-                else:
-                    raise ValueError(
-                        f"Unexpected format in bedmethyl file: {row} contains {pileup_basemod} which cannot be parsed."
-                    )
-                # TODO: consolidate the above into a function; just do adding outside
-                if keep_basemod:
-                    pileup_info = tabix_fields[9].split(" ")
-                    valid_base_counts = int(pileup_info[0])
-                    modified_base_counts = int(pileup_info[2])
-                    if valid_base_counts > 0:
-                        genomic_coord = int(tabix_fields[1])
-                        contig_list.append(contig)
-                        start_list.append(genomic_coord)
-                        end_list.append(genomic_coord + 1)
-                        values_list.append(modified_base_counts / valid_base_counts)
+                )
+                if keep_basemod and valid_in_row > 0:
+                    contig_list.append(contig)
+                    start_list.append(genomic_coord)
+                    end_list.append(genomic_coord + 1)
+                    values_list.append(modified_in_row / valid_in_row)
 
-                        if len(values_list) > chunk_size:
-                            bw.addEntries(
-                                contig_list,  # Contig names
-                                start_list,  # Start positions
-                                ends=end_list,  # End positions
-                                values=values_list,  # Corresponding values
-                            )
-                            contig_list = []
-                            start_list = []
-                            end_list = []
-                            values_list = []
+                    if len(values_list) > chunk_size:
+                        bw.addEntries(
+                            contig_list,  # Contig names
+                            start_list,  # Start positions
+                            ends=end_list,  # End positions
+                            values=values_list,  # Corresponding values
+                        )
+                        contig_list = []
+                        start_list = []
+                        end_list = []
+                        values_list = []
             bw.addEntries(
                 contig_list,  # Contig names
                 start_list,  # Start positions
